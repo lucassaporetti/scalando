526bd74f4ff07da379bae85cbe2a50636218fa5d
Lucas Saporetti <lucassaporetti@gmail.com>
2020-12-03 17:00:03 -0300

    val, var, readLine, readInt, println, if, else

--- Scalando/src/main/scala/demo/tests.scala
+++ Scalando/src/main/scala/demo/tests.scala
@@ -1,7 +1,28 @@
 package demo
+import scala.io.StdIn.{readLine, readInt}
 
 object tests {
   def main(args: Array[String]): Unit = {
-    println("Hello, Lucas!")
+
+    var result = {
+      println("First Operand: ")
+      lazy val first_operand = readInt();
+      println("Second Operand: ")
+      lazy val second_operand = readInt();
+      first_operand + second_operand
+    }
+
+    println(result)
+
+    val name = readLine("What's your name? ")
+    println(s"Hello, \n$name!")
+
+    if (name == "Lucas") {
+      println("Lucasiiiii!")
+    }
+    else {
+      println("Não é o Lucasii!")
+    }
+
   }
 }
