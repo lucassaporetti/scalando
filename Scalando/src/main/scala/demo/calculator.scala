6e85849c1656477d17b49327600567f03a8231a0
Lucas Saporetti <lucassaporetti@gmail.com>
2020-12-05 13:20:16 -0300

    better way to create functions

--- Scalando/src/main/scala/demo/calculator.scala
+++ Scalando/src/main/scala/demo/calculator.scala
@@ -5,15 +5,10 @@ object calculator {
   def main(args: Array[String]): Unit = {
   println("--------- SCALA CALCULATOR ---------")
 
-    def calculate(): Unit = {
+    def get_operation(): Unit = {
 
       val first_operand = {
         println("First Operand: ")
-        readFloat()
-      }
-
-      val second_operand = {
-        println("Second Operand: ")
         readFloat()
       }
 
@@ -22,20 +17,30 @@ object calculator {
         readLine()
       }
 
+      val second_operand = {
+        println("Second Operand: ")
+        readFloat()
+      }
+
+      def sum(first_operand: Float, second_operand: Float): Float = first_operand + second_operand
+      def subtract(first_operand: Float, second_operand: Float): Float = first_operand - second_operand
+      def multiply(first_operand: Float, second_operand: Float): Float = first_operand * second_operand
+      def divide(first_operand: Float, second_operand: Float): Float = first_operand / second_operand
+
       if (operator == "+") {
-        val result = {first_operand + second_operand}
+        val result = sum(first_operand, second_operand)
         println(s"Result: $first_operand + $second_operand = $result")
       }
       if (operator == "-") {
-        val result = {first_operand - second_operand}
+        val result = subtract(first_operand, second_operand)
         println(s"Result: $first_operand - $second_operand = $result")
       }
       if (operator == "*") {
-        val result = {first_operand * second_operand}
+        val result = multiply(first_operand, second_operand)
         println(s"Result: $first_operand * $second_operand = $result")
       }
       if (operator == "/") {
-        val result = {first_operand / second_operand}
+        val result = divide(first_operand, second_operand)
         println(s"Result: $first_operand / $second_operand = $result")
 
 //      var result_list = List()
@@ -65,17 +70,18 @@ object calculator {
       println("We catch 10!")
     }
 
-    val a_number = 12
+    val a_number = 18
 
     a_number match{
       case 12 => println("Cirão da massa!!!")
-      case _ => println("Não é o Cirão!")
+      case 13 | 45 | 17 => println("Vagabonds!")
+      case _ => println("Default value!")
     }
 
     var continue : String = "S"
 
     do {
-      calculate()
+      get_operation()
       continue = readLine("Do you want to continue? [Y/N]: ").strip().toUpperCase()
     } while(continue != "N")
 
