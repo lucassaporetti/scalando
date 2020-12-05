de73ef88c24f26b2bda2e4601ecd296d06a6640c
Lucas Saporetti <lucassaporetti@gmail.com>
2020-12-05 14:19:05 -0300

    beautiful function without ifs

--- Scalando/src/main/scala/demo/calculator.scala
+++ Scalando/src/main/scala/demo/calculator.scala
@@ -5,83 +5,33 @@ object calculator {
   def main(args: Array[String]): Unit = {
   println("--------- SCALA CALCULATOR ---------")
 
-    def get_operation(): Unit = {
+    def get_operation() {
 
-      val first_operand = {
-        println("First Operand: ")
-        readFloat()
-      }
+      println("First Operand: ")
+      val first_operand = {readFloat()}
 
-      val operator = {
-        println("Operator [+ - * /]: ")
-        readLine()
-      }
+      println("Operator [+ - * /]: ")
+      val operator = {readLine()}
 
-      val second_operand = {
-        println("Second Operand: ")
-        readFloat()
-      }
+      println("Second Operand: ")
+      val second_operand = {readFloat()}
 
-      def sum(first_operand: Float, second_operand: Float): Float = {
-        return first_operand + second_operand
-      }
+      def sum(first_operand: Float, second_operand: Float): Float = {return first_operand + second_operand}
 
-      def subtract(first_operand: Float, second_operand: Float): Float =
-        first_operand - second_operand
+      def subtract(first_operand: Float, second_operand: Float): Float = first_operand - second_operand
 
       def multiply(first_operand: Float, second_operand: Float): Float = first_operand * second_operand
 
       def divide(first_operand: Float, second_operand: Float) = first_operand / second_operand
 
-      if (operator == "+") {
-        val result = sum(first_operand, second_operand)
-        println(s"Result: $first_operand + $second_operand = $result")
-      }
-      if (operator == "-") {
-        val result = subtract(first_operand, second_operand)
-        println(s"Result: $first_operand - $second_operand = $result")
-      }
-      if (operator == "*") {
-        val result = multiply(first_operand, second_operand)
-        println(s"Result: $first_operand * $second_operand = $result")
-      }
-      if (operator == "/") {
-        val result = divide(first_operand, second_operand)
-        println(s"Result: $first_operand / $second_operand = $result")
+      val result = operator match {
+        case "+" => sum(first_operand, second_operand)
+        case "-" => subtract(first_operand, second_operand)
+        case "*" => multiply(first_operand, second_operand)
+        case "/" => divide(first_operand, second_operand)
+        case _ => println("This is not a valid operator")}
 
-//      var result_list = List()
-//      result_list.appended
-//      for (operation <- result_list) {
-//        println(s"Result list appends new operation $operation")
-//        }
-
-      }
-    }
-
-    for (x <- 1 to 5) {
-      println("x using to " + x)
-    }
-
-    for (y <- 1 until 6) {
-      println("y using until " + y)
-    }
-
-    for (z <- 1 to 5; w <- 1 until 6) {
-      println("z using to " + z + " w using until " + w)
-    }
-
-    val a_list = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-
-    for {number <- a_list; if number == 10} yield {
-      println("We catch 10!")
-    }
-
-    val a_number = 18
-
-    a_number match{
-      case 12 => println("Cirão da massa!!!")
-      case 13 | 45 | 17 => println("Vagabonds!")
-      case _ => println("Default value!")
+      println(s"Result: $first_operand $operator $second_operand = $result")
     }
 
     var continue : String = "S"
@@ -93,6 +43,28 @@ object calculator {
 
     val name = readLine("What's your name? ")
     println(s"Good bye, $name!")
-
   }
 }
+
+//    for (x <- 1 to 5) {
+//      println("x using to " + x)
+//    }
+//
+//    for (y <- 1 until 6) {
+//      println("y using until " + y)
+//    }
+//
+//    for (z <- 1 to 5; w <- 1 until 6) {
+//      println("z using to " + z + " w using until " + w)
+//    }
+//
+//    val a_list = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+//
+//    for {number <- a_list; if number == 10} yield {println("We catch 10!")}
+//
+//    val a_number = 18
+//
+//    a_number match{
+//      case 12 => println("Cirão da massa!!!")
+//      case 13 | 45 | 17 => println("Vagabonds!")
+//      case _ => println("Default value!")}
